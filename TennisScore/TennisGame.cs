03e02f4270fb4e89f6a4477df897d99f1d702636
thirmoon@gmail.com <thirmoon@gmail.com>
2018-06-09 21:54:51 +0800

    refactor

--- TennisScore/TennisGame.cs
+++ TennisScore/TennisGame.cs
@@ -26,16 +26,8 @@ namespace TennisScore
             {
                 return scoreLookup[game.FirstPlayerScore] + " " + scoreLookup[game.SecondPlayerScore];
             }
-            if (game.FirstPlayerScore == 2)
-            {
-                return "Thirty All";
-            }
-            if (game.FirstPlayerScore == 1)
-            {
-                return "Fifteen All";
-            }
-
-            return "Love All";
+            return scoreLookup[game.FirstPlayerScore] + " All";
+         
         }
     }
 }
