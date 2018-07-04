553bccc356f3f231bbe9fa388cd19c2c1c2f692f
Cloud <thirmoon@gmail.com>
2018-07-04 10:38:31 +0800

    test commit

--- TennisScore/Game.cs
+++ TennisScore/Game.cs
@@ -5,6 +5,7 @@ namespace TennisScore
 {
     public class Game
     {
+		
         public int SecondPlayerScore { get; set; }
         public int FirstPlayerScore { get; set; }
         public int Id { get; set; }
